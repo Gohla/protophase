5878282d4f1c1911a84ab1939a5d99f30ec3aa7b
Gabriël Konat <gabrielkonat@gmail.com>
2011-08-23 16:43:33 +0200

    Added zmq dll for win32. Added simple zmq hello world example to test projects.

--- Mono/TestServiceClient/Main.cs
+++ Mono/TestServiceClient/Main.cs
@@ -1,10 +1,27 @@
 using System;
+using System.Text;
+using ZMQ;
 
 namespace TestServiceClient {
-class MainClass {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+    class MainClass {
+        public static void Main(string[] args) {
+            // ZMQ Context
+            using (Context context = new Context(1)) {
+                //  Socket to talk to server
+                using (Socket requester = context.Socket(SocketType.REQ)) {
+                    requester.Connect("tcp://localhost:5555");
+
+                    string request = "Hello";
+                    for (int requestNbr = 0; requestNbr < 10; requestNbr++) {
+                        Console.WriteLine("Sending request {0}...", requestNbr);
+                        requester.Send(request, Encoding.Unicode);
+
+                        string reply = requester.Recv(Encoding.Unicode);
+                        Console.WriteLine("Received reply {0}: {1}", requestNbr, reply);
+                    }
+                }
+            }
+        }
     }
-}
 }
 
--- Mono/TestServiceServer/Main.cs
+++ Mono/TestServiceServer/Main.cs
@@ -1,10 +1,31 @@
 using System;
+using System.Text;
+using System.Threading;
+using ZMQ;
 
 namespace TestServiceServer {
-class MainClass {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+    class MainClass {
+        public static void Main(string[] args) {
+            // ZMQ Context
+            using(Context context = new Context(1)) {
+                // Socket to talk to clients
+                using(Socket socket = context.Socket(SocketType.REP)) {
+                    socket.Bind("tcp://*:5555");
+                    
+                    while(true) {
+                        // Wait for next request from client
+                        string message = socket.Recv(Encoding.Unicode);
+                        Console.WriteLine("Received request: {0}", message);
+                        
+                        // Do Some 'work'
+                        Thread.Sleep(1000);
+                        
+                        // Send reply back to client
+                        socket.Send("World", Encoding.Unicode);
+                    }
+                }
+            }
+        }
     }
-}
 }
 
