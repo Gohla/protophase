87cecc0ebee97affb4e0cb0908ea0e7cdc8dbcf4
Gabriël Konat <gabrielkonat@gmail.com>
2011-08-23 16:01:16 +0200

    Added clrzmq2 library source. Added test client and server skeleton. Made references between projects.

---
+++ Mono/TestServiceClient/Main.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TestServiceClient {
+class MainClass {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
+
---
+++ Mono/TestServiceServer/Main.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TestServiceServer {
+class MainClass {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
+
